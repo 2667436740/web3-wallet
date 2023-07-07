5e86aa616aa301eaec2ad54cf26b7473337bc9d1
2667436740 <2667436740@qq.com>
2023-07-07 15:04:29 +0800

    账户高频api

--- src/components/AccountSyx.vue
+++ src/components/AccountSyx.vue
@@ -1,12 +1,26 @@
 <template>
   <h1>助记词</h1>
   <p>{{ mnemonic }}</p>
+  <h1>路径</h1>
+  <p>{{ path }}</p>
+  <h1>钱包地址</h1>
+  <p>{{ walletInfo.address }}</p>
+  <h1>钱包私钥</h1>
+  <p>{{ walletInfo.privateKey }}</p>
 </template>
 
 <script setup>
 import { reactive, toRefs, ref } from "vue";
 import * as bip39 from "bip39";
 import * as eWallet from "ethereumjs-wallet";
+console.log(eWallet);
+import Web3 from "web3";
+import { Buffer } from "buffer";
+var web3 = new Web3(
+  Web3.givenProvider ||
+    "wss://mainnet.infura.io/ws/v3/78ba409e93434835a7e2bf1f675b1a40"
+);
+
 //创建助记词
 // const mnemonic = ref("");
 // mnemonic.value = bip39.generateMnemonic();
@@ -14,25 +28,57 @@ import * as eWallet from "ethereumjs-wallet";
 const mnemonic = ref(
   "kid school drill better net tone arch among slot exclude food cliff"
 );
+const path = ref("m/44'/60'/0'/0/0");
+const walletInfo = reactive({
+  address: "",
+  privateKey: "",
+});
 
 //生成密钥对 keypair
 const genMnemonic = async () => {
   const seed = await bip39.mnemonicToSeed(mnemonic.value);
   // console.log(seed);
   const hdWallet = eWallet.hdkey.fromMasterSeed(seed);
-  const keypair = hdWallet.derivePath("m/44'/60'/0'/0/0");
+  const keypair = hdWallet.derivePath(path.value);
   // console.log(keypair);
 
-  //获取私钥
   //1.获取钱包对象
   const wallet = keypair.getWallet();
+  // console.log(wallet);
   //2.获取钱包地址
   const loweraseAddress = wallet.getAddressString();
+  // console.log(loweraseAddress);
+  walletInfo.address = loweraseAddress;
   //3.获取钱包校验地址
   const checkAddress = wallet.getChecksumAddressString();
   //4.获取私钥
   const priKey = wallet.getPrivateKey().toString("hex");
-  console.log(priKey);
+  // console.log(priKey);
+  walletInfo.privateKey = priKey;
+
+  //私钥 → keystore ( web3js或wallet对象)
+  //1.web3js
+  // const keyStore1 = web3.eth.accounts.encrypt(walletInfo.privateKey, "111111");
+  // console.log(JSON.stringify(keyStore1));
+  //2.wallet对象
+  // 从私钥创建钱包对象
+  // const keyStore2 = await wallet.toV3("111111");
+  // console.log(keyStore2);
+
+  //keystore → 私钥 ( web3js或wallet对象)
+  //1.web3js
+  // const res1 = web3.eth.accounts.decrypt(keyStore1, "111111");
+  // console.log(res1);
+  //2.wallet对象
+  // const wallet2 = await eWallet.fromV3(keyStore2, "111111");
+  // const key = wallet2.getPrivateKey().toString("hex");
+  // console.log(key);
+
+  //通过私钥获取地址
+  // const bufferPrikey = Buffer(priKey, "hex");
+  // const wallet3 = eWallet.default.fromPrivateKey(bufferPrikey);
+  // const loweraseAddress2 = wallet3.getAddressString();
+  // console.log(bufferPrikey, wallet3, loweraseAddress2);
 };
 genMnemonic();
 </script>
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import Components from "unplugin-vue-components/vite";
 import { VantResolver } from "unplugin-vue-components/resolvers";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
-
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -12,6 +12,7 @@ export default defineConfig({
     Components({
       resolvers: [VantResolver()],
     }),
+    rollupNodePolyFill(),
   ],
   define: {
     "process.env": {},
@@ -19,9 +20,39 @@ export default defineConfig({
   resolve: {
     alias: {
       web3: "web3",
-      assert: "assert",
       buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
       process: "rollup-plugin-node-polyfills/polyfills/process-es6",
+      crypto: "rollup-plugin-node-polyfills/polyfills/crypto-browserify",
+      util: "rollup-plugin-node-polyfills/polyfills/util",
+      sys: "util",
+      events: "rollup-plugin-node-polyfills/polyfills/events",
+      stream: "rollup-plugin-node-polyfills/polyfills/stream",
+      path: "rollup-plugin-node-polyfills/polyfills/path",
+      querystring: "rollup-plugin-node-polyfills/polyfills/qs",
+      punycode: "rollup-plugin-node-polyfills/polyfills/punycode",
+      url: "rollup-plugin-node-polyfills/polyfills/url",
+      // string_decoder: "rollup-plugin-node-polyfills/polyfills/string-decoder",
+      http: "rollup-plugin-node-polyfills/polyfills/http",
+      https: "rollup-plugin-node-polyfills/polyfills/http",
+      os: "rollup-plugin-node-polyfills/polyfills/os",
+      assert: "rollup-plugin-node-polyfills/polyfills/assert",
+      constants: "rollup-plugin-node-polyfills/polyfills/constants",
+      _stream_duplex:
+        "rollup-plugin-node-polyfills/polyfills/readable-stream/duplex",
+      _stream_passthrough:
+        "rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough",
+      _stream_readable:
+        "rollup-plugin-node-polyfills/polyfills/readable-stream/readable",
+      _stream_writable:
+        "rollup-plugin-node-polyfills/polyfills/readable-stream/writable",
+      _stream_transform:
+        "rollup-plugin-node-polyfills/polyfills/readable-stream/transform",
+      timers: "rollup-plugin-node-polyfills/polyfills/timers",
+      console: "rollup-plugin-node-polyfills/polyfills/console",
+      vm: "rollup-plugin-node-polyfills/polyfills/vm",
+      zlib: "rollup-plugin-node-polyfills/polyfills/zlib",
+      tty: "rollup-plugin-node-polyfills/polyfills/tty",
+      domain: "rollup-plugin-node-polyfills/polyfills/domain",
     },
   },
   optimizeDeps: {
@@ -35,8 +66,17 @@ export default defineConfig({
         NodeGlobalsPolyfillPlugin({
           process: true,
           buffer: true,
+          crypto: true,
         }),
         NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin used during production bundling
+        rollupNodePolyFill(),
       ],
     },
   },
